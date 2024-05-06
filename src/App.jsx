42c132fe0edc12e98eaf9f4781133b3d79ad6102
mridul891 <58576921+mridul891@users.noreply.github.com>
2024-05-06 07:34:19 +0530

    Login Pager Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,20 @@
-import { useEffect, useState } from 'react'
 
 import './App.css'
 import GitHubOAuth from './Component/GithubLogin';
-
+import Landing from './Component/Landing';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-  const [result, setresult] = useState([]);
-  const [repo, setrepo] = useState(null);
-  const CLIENT_ID = '5db7bf02bf06b827e134';
-  const REDIRECT_URI = 'http://localhost:5173';
-  const SCOPES = ['user', 'repo', 'repo:private'];
-
-
-  const handlerepos = async () => {
-    const options = {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json"
-      }
-    }
-    try {
-      const response = await fetch('http://localhost:3000/get-repos', options)
-      const data = await response.json();
-      const member = data.items
-      setresult(member)
-      console.log(result)
-    }
-
-    catch (error) {
-      console.log(error)
-    }
-  }
-
-  const generateaccessToken = async () => {
-    await fetch('http://localhost:3000/callback').then(response => response.json()).then(data => console.log(data.access_token))
-  }
-  const loginWithGithub = () => {
-    // await window.location.assign("https://github.com/login/oauth/authorize?client_id=" + CLIENT_ID + "&scope=" + SCOPES + "redirect_uri=" + REDIRECT_URI)
-    fetch('http://localhost:3000/login', { mode: "no-cors" }, {
-      method: "GET",
-      headers: {
-        "Accept": "application/json"
-      }
-    });
-  }
 
   return (
-    <div className='h'>
-      <h1>GitHub OAuth with React</h1>
-      <GitHubOAuth />
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/login" element={<GitHubOAuth />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   )
 }
 
--- src/Component/GithubLogin.jsx
+++ src/Component/GithubLogin.jsx
@@ -1,23 +1,85 @@
+import { Button } from "@/components/ui/button";
 
-// const GITHUB_CLIENT_ID = '5db7bf02bf06b827e134';
-// const GITHUB_CLIENT_SECRET = "your_client_secret";
-// const GITHUB_CALLBACK_URL = 'http://localhost:5173';
-// const SCOPES = 'user ,repo ,repo:private';
+import logo1 from '../assets/logo1.svg'
+import { Link, redirect, useNavigate } from "react-router-dom";
+import { Icons } from "./icons";
+import { useEffect, useState } from "react";
+
+
 const githubOAuthURL = `http://localhost:3000/login`;
 
-const handlesubmit = async () => {
-    const a = await fetch("http://localhost:3000/login", { mode: "no-cors" }, {
-        method: "GET",
-    })
-    console.log(a)
-}
+
+
 const GitHubOAuth = () => {
 
+    const [url, setUrl] = useState("http://localhost:3000/login")
+    const [accessToken, setAccessToken] = useState(null)
+    const navigate = useNavigate()
+    useEffect(() => {
+        const url = window.location.search;
+        const parsedData = new URLSearchParams(url);
+        const accessToken = parsedData.get("code");
+        console.log(accessToken)
+        const options = {
+            method: "GET"
+        }
+        try {
+            fetch("http://localhost:3000/callback" + "?code=" + accessToken, options).then((response) => response.json()).then((data) => {
+                setAccessToken(data.access_token)
+                if (data.access_token) {
+                    localStorage.setItem("accessToken", data.access_token)
+                }
+                if (accessToken != null) {
+                    navigate("/")
+                }
+            })
+        } catch (error) {
+            console.log(error)
+        }
+
+    })
 
     return (
-        <div>
-
-            <button onClick={handlesubmit}>getaccesstoken</button>
+        <div className="grid grid-cols-2 h-[100vh] w-[100vw]">
+            {/* One part */}
+            <div className="bg-[#202024] flex items-center justify-center">
+                <div className="py-[7rem] px-20 ">
+                    <Link to="/">
+                        <img src={logo1} alt="" />
+                    </Link>
+                </div>
+            </div>
+            {/* Second Half  */}
+            <div className="bg-[#000] flex items-center justify-center">
+                {/* <Button onClick={handleclick}>getaccesstoken</Button> */}
+                <div className="lg:p-8">
+                    <div className="mx-auto flex w-full flex-col justify-center space-y-6 gap-10 sm:w-[350px]">
+                        <div><h1 className="text-white font-bold text-4xl">Welcome Back ✌️</h1></div>
+                        <div className=" bg-white font-semibold flex justify-center rounded-md">
+                            <a href={url} className="flex items-center py-2 ">
+                                <Icons.gitHub className="mr-2 h-4 w-4" /> Sign In With GitHub
+                            </a>
+                        </div>
+                        <p className="px-8 text-center text-sm text-muted-foreground">
+                            By clicking continue, you agree to our{" "}
+                            <Link
+                                href="/terms"
+                                className="underline underline-offset-4 hover:text-primary"
+                            >
+                                Terms of Service
+                            </Link>{" "}
+                            and{" "}
+                            <Link
+                                href="/privacy"
+                                className="underline underline-offset-4 hover:text-primary"
+                            >
+                                Privacy Policy
+                            </Link>
+                            .
+                        </p>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
