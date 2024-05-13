0067eb70af2aa3303b04022453ab65f63ee6fb7c
mridul891 <58576921+mridul891@users.noreply.github.com>
2024-05-13 22:32:31 +0530

    Changes in dashboard

--- src/Component/Dashboard.jsx
+++ src/Component/Dashboard.jsx
@@ -1,11 +1,11 @@
 import { username } from "@/Globalstate";
 import { useEffect, useState } from "react"
-import { useRecoilValue, useSetRecoilState } from "recoil";
+import { useSetRecoilState } from "recoil";
 
 
 const Dashboard = () => {
     const [info, setInfo] = useState({});
-    const setvalue = useSetRecoilState(username)
+    // const setvalue = useSetRecoilState(username)
     useEffect(() => {
         const apikey = localStorage.getItem("accessToken");
         console.log(apikey)
@@ -17,12 +17,10 @@ const Dashboard = () => {
         }
         fetch('http://localhost:3000/get-user', options)
             .then(response => response.json())
-            .then(data => {
-                setvalue(data.login)
-            })
+            .then(data => setInfo(data))
     }, [])
     return (
-        <>
+        <>  
             <div>{info.login}</div>
             <img src={info.avatar_url} alt="profile photo" />
         </>
